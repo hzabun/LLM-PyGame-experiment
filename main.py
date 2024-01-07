482b55bb40631db978bee14760037dc9589cd4bd
hazab <halil.zabun@icloud.com>
2024-01-07 09:58:22 +0100

    Add: character.py and render.py to import and instantiate accordingly

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
 import pygame
-from menu import Menu  # assuming you have menu.py file where Menu class is defined
-from level import Level  # assuming you have level.py file where Level class is defined
+from menu import Menu  # Import Menu class from menu module
+from level import Level  # Import Level class from level module
+from character import Character  # Import Character class from character module
+from render import Render  # Import Render class from render module
+
+# Pygame initialization
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
 
 
 class Game:
     def __init__(self):
         self.state = 'initialized'
-        self.screen_width = 1200
-        self.screen_height = 800
-
-        pygame.init()
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.menu_screen = None
+        self.level = None
+        self.character = None
+        self.render = None
 
     def run(self):
         while True:
             if self.state == 'initialized':
                 self.state = 'main_menu'
-
             elif self.state == 'main_menu':
-                menu = Menu(self.screen)
-                menu_screen = menu.run()  # assuming the run function returns something related to menu
+                menu = Menu(screen)
+                self.menu_screen = menu.run()
                 self.state = 'level_generation'
-
             elif self.state == 'level_generation':
-                level = Level(menu_screen)
-                level_screen = level.generate_level()  # assuming this function return something about the level
-                self.state = 'quitting'
+                self.level = Level(self.menu_screen)
+                self.character = Character()  # Instantiate Character class
+                self.state = 'rendering'
+            elif self.state == 'rendering':
+                self.render = Render(self.level.screen, self.level.platforms, self.character)
+                self.render.run()
+                self.state = 'quitting'  # Change the state to quit the game
+            else:
+                break  # Quit the game
 
-            elif self.state == 'quitting':
-                pygame.quit()
 
 if __name__ == "__main__":
     Game().run()
